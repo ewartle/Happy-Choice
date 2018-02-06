ab39fe0d7f144cdf1626f81c4bd73402343499b5
jshockley99 <jbshockley99@gmail.com>
2018-02-06 14:30:50 -0500

    initial form created

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Survey from "./pages/Survey/Survey";
+import Form from "./pages/Form/Form";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <p> It's working!</p>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+const App = () =>
+ <Router>
+   <div>
+   
+     <Switch>
+       <Route exact path="/Survey" component={Survey} />
+       <Route exact path="/Form" component={Form} />
+     
+     </Switch>
+   </div>
+ </Router>;
+
 
 export default App;
