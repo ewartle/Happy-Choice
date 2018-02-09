f60746a34a2350206e238b5efae025d0befae336
pperreiah <31325721+pperreiah@users.noreply.github.com>
2018-02-08 20:38:35 -0500

    changed require stmt in server.js to renameded 'calculate.js' file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const calculateSurveyResults = require('./algorithm-code.js')
+const calculateSurveyResults = require('./calculate.js')
 const { Admin, Survey, Choice, Participant } = require('./models/database.js');
 
 
